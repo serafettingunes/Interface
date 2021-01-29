b835151a2045c94db5cf31386c339930901821c0
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-29 22:56:53 +0300

    Add project files.

---
+++ Interface/ICustomerDal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+
+    }
+
+    class SqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Update");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Delete");
+        }
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Update");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Delete");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal iCustomerDal)
+        {
+            iCustomerDal.Add();
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewMethod();
+
+            //NewMethod1();
+
+
+            ICustomerDal[] iCustomerDals = new ICustomerDal[2]
+            {
+                new OracleCustomerDal(),
+                new SqlCustomerDal()
+
+            };
+
+            foreach (var itemCustomerDal in iCustomerDals)
+            {
+                itemCustomerDal.Add();
+            }
+        }
+
+        private static void NewMethod1()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void NewMethod()
+        {
+            PersonManager personManager = new PersonManager();
+            personManager.Add(new Student() { Id = 4, Ad = "Enes", Soyad = "Güneş", Not = 100 });
+            personManager.Add(new Worker() { Id=5,Ad="Halil",Soyad="Güneş", });
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string Ad { get; set; }
+        string Soyad { get; set; }
+
+
+    }
+
+    class Student : IPerson
+    {
+      public  int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Not { get; set; }
+
+
+    }
+    class Musteri : IPerson
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Para { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.Ad+" "+person.Soyad);
+        }
+    }
+}
