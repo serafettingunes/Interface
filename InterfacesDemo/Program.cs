8c289448f95255e0153138d613f30d3e319eb97b
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-30 20:12:35 +0300

    değişti

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorked[] iWorkeds = new IWorked[3]
+            {
+                new Robot(),
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var item  in iWorkeds)
+            {
+                item.Work();
+            }
+        }
+    }
+
+    interface IWorked
+    {
+         void Work();
+
+
+
+    }
+
+    interface IEated
+    {
+         void Eat();
+
+
+
+    }
+
+    interface ISalary
+    {
+        void Salary();
+
+
+
+
+    }
+
+    class Robot : IWorked
+    {
+        public void Work()
+        {
+            
+        }
+    }
+
+    class Worker: IWorked,ISalary,IEated
+    {
+        public void Work()
+        {
+            
+        }
+
+        public void Salary()
+        {
+            
+        }
+
+        public void Eat()
+        {
+
+        }
+    }
+
+    class Manager:IWorked,ISalary,IEated
+    {
+        public void Work()
+        {
+
+        }
+
+        public void Salary()
+        {
+            
+        }
+
+        public void Eat()
+        {
+            
+        }
+    }
+}
---
+++ Kalitim/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Kalitim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Person{Ad = "Kaan"},
+                new Customer{Ad = "Şerafettin",CustomerOzellik = "Yönetici",Id = 4,Soyad = "Güneş"},
+                new Student{Ad = "Ali"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Ad);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+    }
+
+    class Student : Person
+    {
+        public string Departman { get; set; }
+        
+    }
+
+    class Customer : Person
+    {
+        public string CustomerOzellik { get; set; }
+    }
+
+
+}
